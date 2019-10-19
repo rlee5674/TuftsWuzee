863b93dab0e5a522881aea80235ba4916d134cd9
Janny Huang <jannyhuang12@gmail.com>
2019-10-19 18:21:19 -0400

    created template for pages and navigation bar

---
+++ js/nav.js
@@ -0,0 +1,12 @@
+function fill_navigation() {
+    document.getElementById("nav-bar").innerHTML =
+        "<ul>\
+            <li><a href=\"index.html\">Home</a></li> \
+            <li><a href=\"mission.html\">Mission Statement</a></li> \
+            <li><a href=\"dances.html\">Current Dances</a></li> \
+            <li><a href=\"costumes.html\">Costumes/Props</a></li> \
+            <li><a href=\"media.html\">Media</a></li> \
+            <li><a href=\"calendar.html\">Calendar</a></li> \
+            <li><a href=\"contact.html\">Contact</a></li> \
+        </ul>"
+}
